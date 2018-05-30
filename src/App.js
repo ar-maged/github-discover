ed560ed69dae79a735cf16892f039a7a0dd88883
A.R. Maged <abdulrahman.maged95@gmail.com>
2018-05-30 04:14:43 +0200

    :sparkles: Render most starred repos of all time

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 import { mostStarredOfAllTime } from './github';
 
 class App extends Component {
+  state = {
+    mostStarredOfAllTime: [],
+  };
+
   async componentDidMount() {
-    console.log(await mostStarredOfAllTime());
+    this.setState({
+      mostStarredOfAllTime: await mostStarredOfAllTime(),
+    });
+  }
+
+  _renderRepo(repo) {
+    return (
+      <div>
+        <h5>{repo.name}</h5>
+        <p>{repo.description}</p>
+      </div>
+    );
   }
 
   render() {
-    return <h1>GitHub Discover</h1>;
+    return (
+      <div>
+        <h2>GitHub Discover</h2>
+        <p>{this.state.mostStarredOfAllTime.map(this._renderRepo)}</p>
+      </div>
+    );
   }
 }
 
